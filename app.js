aaaca472d3ba14a59728554facf293f3ff9f8576
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-06 10:42:33 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('it works');
+});
+
+app.listen(port, () => {
+    console.log(`app running on http://localhost:${port}`);
+
+})
