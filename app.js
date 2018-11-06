4569e5034e07206b91be0334fc39aae6da80ae85
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-06 12:36:50 +0530

    add google route to app.js

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+//passport config
+require('./config/passport')(passport);
+//load routes
+const auth = require('./routes/auth');
 
 const app = express();
-
-const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('it works');
 });
+
+app.use('/auth', auth);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`app running on http://localhost:${port}`);
