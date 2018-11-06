f46f068fab712dcef6501c3abe2b8dfd8b0c4a6e
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-06 13:07:50 +0530

    update

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: 'mongodb://root:0987imgmp@ds153593.mlab.com:53593/storybooks-dev',
+    googleClientId: '261564545184-eulqc1u2b5lkrdh10ia3ev4k0pvb1nod.apps.googleusercontent.com',
+    googleClientSecret: '7uTPOUZSPPUmzfL1Z4t-hD3O'
+
+}
