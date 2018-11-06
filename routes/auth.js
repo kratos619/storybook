2c5d14c687f0fc2f776336714bea27f3cc13c21d
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-06 13:02:58 +0530

    make successfull auth in console

--- routes/auth.js
+++ routes/auth.js
@@ -6,5 +6,14 @@ router.get('/google', passport.authenticate('google', {
     scope: ['profile', 'email']
 }));
 
+router.get('/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/',
+    }),
+    (req, res) => {
+        // Successful authentication, redirect home.
+        res.redirect('/dashboard');
+    });
+
 
 module.exports = router;
