3f0a0b13d0ba2f6f9200afbdc27661801415b28c
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-08 11:56:30 +0530

    create Routes for dashboard and render it

--- routes/stories.js
+++ routes/stories.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index/welcome');
-});
+    res.render('stories/index');
+})
 
-router.get('/dashboard', (req, res) => {
-  res.render('index/dashboard');
-});
+
 module.exports = router;
