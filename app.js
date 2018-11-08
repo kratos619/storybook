d31e406e28fefef47bb694f9a6ff600c6b9fa0c1
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-08 07:44:08 +0530

    add passport ititialize and passport session middkewares  add session above auth router

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookiesParser = require('cookie-parser');
+const session = require('express-session');
 const passport = require('passport');
+
+// load user model
+require('./models/user');
 //passport config
 require('./config/passport')(passport);
 //load routes
@@ -8,25 +13,41 @@ const auth = require('./routes/auth');
 
 const app = express();
 
-//load keys 
+//load keys
 const keys = require('./config/keys');
 
 //mongoose connect
-mongoose.connect(keys.mongoURI, {
-    useNewUrlParser: true
-}).then(() => {
+mongoose
+  .connect(
+    keys.mongoURI,
+    {
+      useNewUrlParser: true
+    }
+  )
+  .then(() => {
     console.log('db connect');
-}).catch(err => console.log(err));
+  })
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-    res.send('it works');
+  res.send('it works');
 });
+app.use(cookiesParser());
+app.use(
+  session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true
+  })
+);
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`app running on http://localhost:${port}`);
-
-})
+  console.log(`app running on http://localhost:${port}`);
+});
