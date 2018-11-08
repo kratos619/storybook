08e50b58ab510fa03027434c0258b8eee3b7bff5
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-08 07:37:58 +0530

    create new user by google auths

--- config/passport.js
+++ config/passport.js
@@ -2,16 +2,64 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mangoose = require('mongoose');
 const keys = require('./keys');
 
-module.exports = function (passport) {
-    passport.use(
-        new GoogleStrategy({
-            clientID: keys.googleClientId,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback',
-            proxy: true
-        }, (accessToken, refreshToken, profile, done) => {
-            console.log(accessToken);
-            console.log(profile);
-        })
+// load user models
+
+const User = mangoose.model('users');
+
+module.exports = function(passport) {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: keys.googleClientId,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback',
+        proxy: true
+      },
+      (accessToken, refreshToken, profile, done) => {
+        // console.log(accessToken);
+        console.log(profile);
+
+        // setup image variable and remove 'size?= ' from google image
+        const image = profile.photos[0].value.substring(
+          0,
+          profile.photos[0].value.indexOf('?')
+        );
+        console.log(image);
+
+        // create New Users
+        const newUser = {
+          googleID: profile.id,
+          firstname: profile.name.givenName,
+          lastname: profile.name.familyName,
+          email: profile.emails[0].value,
+          image: image
+        };
+
+        // check for existing user
+        User.findOne({
+          googleID: profile.id
+        }).then(user => {
+          if (user) {
+            // return user
+            done(null, user);
+          } else {
+            // create User
+            new User(newUser).save().then(user => done(null, user));
+          }
+        });
+      }
     )
-}
+  );
+
+  // serialize users
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  // deserialize user
+  passport.deserializeUser((user, done) => {
+    User.findById(user.id).then(user => {
+      done(null, user);
+    });
+  });
+};
